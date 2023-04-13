936b022c7a564e1d209c9563c7456a3794368c6b
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2023-04-13 12:33:19 +0200

    Exercice DOM

--- WebAPIs/src/main.ts
+++ WebAPIs/src/main.ts
@@ -1,5 +1,23 @@
 import { createTodo } from "./todo.js";
 
-console.log('Hello');
+const formEl = document.querySelector('.todos-form') as HTMLFormElement;
+const listEl = document.querySelector('.todos-list') as HTMLDivElement;
+const inputEl = document.querySelector('.todos-new-value') as HTMLInputElement;
 
-createTodo();
+formEl.addEventListener('submit', (event) => {
+  event.preventDefault(); // désactive le form
+
+  const divEl = createTodo({
+    id: Math.random(),
+    title: inputEl.value,
+    completed: false,
+  });
+
+  listEl.append(divEl);
+});
+
+/*
+Exercice 3
+Au clic de la checkbox du formulaire
+Cocher ou décocher toutes les checkboxes de .todos-list
+*/
--- WebAPIs/src/todo.ts
+++ WebAPIs/src/todo.ts
@@ -1,3 +1,39 @@
-export function createTodo() {
+type Todo = {
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
+export function createTodo(todo: Todo): HTMLDivElement {
+  // <div class="todos-item" data-todo-id="32">
+  //     <input type="checkbox">
+  //     <span>ABC</span>
+  //     <button>-</button>
+  //   </div>
+  const divEl = document.createElement('div');
+
+  divEl.className = "todos-item";
+  divEl.dataset.todoId = String(todo.id);
+
+  const spanEl = document.createElement('span');
+  spanEl.innerText = todo.title;
+  divEl.append(spanEl);
+
+  /*
+  Exercice 1
+  Compléter ce code de façon à créer le HTML suivant
+  <div class="todos-item" data-todo-id="32">
+    <input type="checkbox">
+    <span>ABC</span>
+    <button>-</button>
+  </div>
+  Il faut que la checkbox soit cochée si todo.completed === true
+  */
+
+  /*
+  Exercice 2
+  Au clic du bouton moins, supprimer la div parent
+  */
+
+  return divEl;
 }
